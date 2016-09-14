fc5dff218a39e698522dc9aa1a92ac8a5b65dd31
Varrenlad Irredail <varrenlad@gmail.com>
2016-09-14 10:11:04 +0600

    Fix #1

--- steep.learning.curve/Source.cpp
+++ steep.learning.curve/Source.cpp
@@ -3,13 +3,99 @@
 #include <windowsx.h>
 #include <stdio.h>
 #include <conio.h>
+
+HDC hdc;
+RECT rt;
+
+struct trapeze {
+	int A[2];
+	int B[2];
+	int C[2];
+	int D[2];
+};
+
+struct colour {
+	int red;
+	int green;
+	int blue;
+};
+
+void draw_contour(trapeze t, HPEN hContourPen) {
+
+	POINT ppt[5] = { {t.A[0], t.A[1]},{ t.B[0], t.B[1] },
+	{ t.C[0], t.C[1] },{ t.D[0], t.D[1] },{ t.A[0], t.A[1] } };
+	Polyline(hdc, ppt, 5);
+}
+
+void draw_filled(trapeze t, HBRUSH hTrapezeBrush) {
+
+}
+
+void draw_partfilled(trapeze outer, trapeze inner, HPEN hContourPen,
+					 HBRUSH hTrapezeBrush, colour cBGColour) {
+
+}
+
+int validate_trapeze(trapeze t) {
+	if  (rt.right < t.A[0] || t.A[0] < 0)
+		return 0;
+	if  (rt.right < t.B[0] || t.B[0] < 0)
+		return 0;
+	if  (rt.right < t.C[0] || t.C[0] < 0)
+		return 0;
+	if  (rt.right < t.D[0] || t.D[0] < 0)
+		return 0;
+	if (rt.bottom < t.A[1] || t.A[1] < 0)
+		return 0;
+	if (rt.bottom < t.B[1] || t.B[1] < 0)
+		return 0;
+	if (rt.bottom < t.C[1] || t.C[1] < 0)
+		return 0;
+	if (rt.bottom < t.D[1] || t.D[1] < 0)
+		return 0;
+	return 1;
+}
+
+int validate_colour(colour c) {
+	if (c.blue < 0  || c.blue > 255)
+		return 0;
+	if (c.green < 0 || c.green > 255)
+		return 0;
+	if (c.red < 0   || c.red > 255)
+		return 0;
+}
+
+//For contoured
+int validate_input(trapeze t, HPEN hContourPen, colour cBGColour) {
+	if (!validate_trapeze(t))
+		return 0;
+}
+
+//For filled
+int validate_input(trapeze t, HBRUSH hTrapezeBrush, colour cBGColour) {
+	if (!validate_trapeze(t))
+		return 0;
+	if (!validate_colour(cBGColour))
+		return 0;
+}
+ 
+//For cut
+int validate_input(trapeze outer, trapeze inner, HPEN hContourPen,
+HBRUSH hTrapezeBrush, colour cBGColour) {
+	if (!validate_trapeze(outer))
+		return 0;
+	if (!validate_trapeze(inner))
+		return 0;
+	if (!validate_colour(cBGColour))
+		return 0;
+}
+
 void main()
 {
 	// получаем идентификатор окна
 	HWND hwnd = GetConsoleWindow();
 	// получаем контекст отображения
-	HDC hdc = GetDC(hwnd);
-	RECT rt;
+	hdc = GetDC(hwnd);
 	char buf[100];
 	// устанавливаем цвет фона
 	SetBkColor(hdc, RGB(255, 10, 10));
@@ -60,3 +146,4 @@ void main()
 	// освобождаем контекст отображения
 	ReleaseDC(hwnd, hdc);
 }
+
