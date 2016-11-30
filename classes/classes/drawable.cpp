0caa2473ba8d32d93e814d062e4a04e3bdf8ab27
Varrenlad Irredail <varrenlad@gmail.com>
2016-11-30 13:25:15 +0700

    Stub

--- classes/classes/drawable.cpp
+++ classes/classes/drawable.cpp
@@ -35,7 +35,7 @@ void Drawable::ToEMF(std::string &filename) {
 	hdc = temp;
 }
 
-void Drawable::Resize(float new_size) {
+void Drawable::Resize(float new_size) { //resize needs accuracy gix
 	size_t i;
 	if (new_size < 0)
 		throw EXC_CANT_CONTAIN;
@@ -45,25 +45,30 @@ void Drawable::Resize(float new_size) {
 	}
 }
 
-void Drawable::Rotate(float angle) { //rotation needs placement fix
+void Drawable::Rotate(float an) { //rotation needs accuracy fix 
 	size_t i;
+	double angle = an/57.3, temp = angle, x, y;
 	POINT def_centre;
+	POINT line[2];
 	if (count_of_p == 4) {
-		float a = abs(points[1].x - points[0].x);
-		float b = abs(points[3].x - points[2].x);
-		float h = abs(points[1].y - points[0].y);
-		def_centre.x = (a*a + a*b + b*b) / (3 * (a + b));//trapezoid
-		def_centre.y = (h*(a + 2 * b)) / (3 * (a + b));
-		SetPixel(hdc, def_centre.x, def_centre.y, RGB(0, 0, 0));
-		_getch();
-		_getch();
+		float a = abs(points[1].x + points[0].x);
+		float h = abs(points[2].y + points[0].y);
+		def_centre.x = a / 2;
+		def_centre.y = h / 2;
 	}
 	else { //rectangle
 		def_centre.x = (points[0].x + points[1].x) / 2;
 		def_centre.y = (points[0].y + points[1].y) / 2;
 	}
 	for (i = 0; i < count_of_p; ++i) {
-		points[i].x = def_centre.x + (points[i].x + def_centre.x) * cos(angle) - (points[i].y + def_centre.y) * sin(angle);
-		points[i].y = def_centre.y + (points[i].y + def_centre.y) * cos(angle) - (points[i].x + def_centre.x) * sin(angle);
+		x = points[i].x;
+		y = points[i].y;
+		//while (angle > 0) {
+			x = def_centre.x + (x - def_centre.x) * std::cos(angle) + (y - def_centre.y) * std::sin(angle);
+			y = def_centre.y + (y - def_centre.y) * std::cos(angle) - (x - def_centre.x) * std::sin(angle);
+			//angle -= (rot > 0) ? 0.1 : -0.1;
+		//}
+		points[i].x = round(x);
+		points[i].y = round(y);
 	}
 }
